a7e5afa7f78ff51f5497ab57233cef65120b0ce4
JongukYang <didwhddnr123@naver.com>
2022-08-23 03:01:36 +0900

    초기 세팅

---
+++ HSU_StudentCouncil/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('login/', views.login, name='login'),
+    # path('logout/', views.logout, name='logout'),
+    # path('signup/', views.signup, name='signup'),
+    # path('forgot_password/', views.forgot_password, name='forgot_password'),
+]
