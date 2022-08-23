96a5fc6b3a8bb81e73b81e3bb93b3c9648749eef
JongukYang <73643657+JongukYang@users.noreply.github.com>
2022-08-23 18:30:00 +0900

    Delete backup directory

--- backup/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-# Register your models here.
-
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('id','realName')
-admin.site.register(UserProfile,UserProfileAdmin)
